884b80b30f5b0dc9e2aa20d1adfe4dfa22f93fd5
FerdyApriliyanto <ferdya7414@gmail.com>
2026-03-26 15:34:05 +0700

    Use clean URLs for Flutter web routing

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:personal_portfolio/app/theme/app_colors.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
 
   runApp(
     GetMaterialApp(
